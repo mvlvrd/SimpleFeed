557e076c1b9fe4c097d3e1e06a7dc34dc8b1c01a
valverde <manolo.valverde@gmail.com>
2026-03-26 20:23:14 +0900

    Move mark-all functionality to the context menu.

--- utils.js
+++ utils.js
@@ -1,29 +1,25 @@
-function getItems({dtListSelector, getter}, text) {
-    const parser = new DOMParser();
-    const dts = parser
-      .parseFromString(text, "text/html")
-      .querySelectorAll(dtListSelector);
-    return Array.from(dts).map(dt => getter(dt));
-}
-
 export async function fetchAndParse(schemaName) {
-    // CONFIG is loaded as a global from config.js via background.xhtml
-    const {url, dtListSelector, getter} = CONFIG[schemaName];
-    return fetch(url)
-    .then(response => {
-        if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.text()})
-        .then(text => getItems({dtListSelector, getter}, text))
+  // CONFIG is loaded as a global from config.js via background.xhtml
+  const {url, dtListSelector, getter} = CONFIG[schemaName];
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) { throw new Error(`HTTP error! Status: ${response.status}`); }
+      return response.text();})
+    .then((text) => {
+      const parser = new DOMParser();
+      const dts = parser
+            .parseFromString(text, "text/html")
+            .querySelectorAll(dtListSelector);
+      return Array.from(dts).map((dt) => getter(dt));
+    })
     .catch(error => {
-        console.error("Fetch failed:", error);
-        browser.notifications.create({
+      console.error("Fetch failed:", error);
+      browser.notifications.create({
         type: "basic",
         iconUrl: "icons/icon-48.png",
         title: `Fetch ${url} failed`,
         message: error.message
-        });
-        throw error;
+      });
+      throw error;
     });
 }
